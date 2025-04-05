ba259e753d68687b9c7537a88b5a2e2affd26519
windy <707298413@qq.com>
2025-04-06 01:03:07 +0800

    add mlua

---
+++ install.sh
@@ -0,0 +1,21 @@
+git submodule init
+git submodule update
+
+OS=`uname`
+
+if [[ $OS == 'Linux' ]]; then
+    PLAT="linux"
+elif [[ $OS == 'Darwin' ]]; then
+    PLAT="macosx"
+else
+    echo "unknown OS"
+    exit 1
+fi
+
+# skynet
+cd skynet && make clean && make ${PLAT} && cd -
+
+# clibs
+cd luaclib && cargo build --release
+ln -s ./target/release/libjson.dylib ./target/bin/json.so
+cd -
