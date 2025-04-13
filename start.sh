e9b53c75af02cf7d521ec6f72fd0146fa06f313a
windy <707298413@qq.com>
2025-04-14 00:26:10 +0800

    update shell

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 export ROOT=$(cd "$(dirname "$0")"; pwd)
 export DAEMON=false
+export THREAD=4
 
 export LOG_LEVEL=1 # 1: trace 2: debug 3: info 4: warn 5: error
 export LOG_LOCATION=true
@@ -13,9 +14,34 @@ if [[ -f skynet.pid ]]; then
     echo "skynet is stopped"
 fi
 
+usage() {
+  echo "usage: $0 [-t thread_num] [-d]"
+  echo "eg:"
+  echo "  $0 -t 8 -d"
+  exit 1
+}
+
 # Parse options
-while getopts ":d" opt; do
-    [[ $opt == "d" ]] && DAEMON=true || { echo "unknown option $opt"; exit 1; }
+while getopts ":t:d:h" opt; do
+    case $opt in
+        t)
+            if ! [[ $OPTARG =~ ^[1-9]+$ ]]; then
+                echo "error: invalid thread num $OPTARG" >&2
+                usage
+            fi
+            THREAD=$OPTARG
+            ;;
+        d)
+            DAEMON=true
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            echo "unknow option $opt"
+            usage
+            ;;
+    esac
 done
 
 # Start skynet
