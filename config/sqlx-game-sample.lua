333ba699bace44c27d22f887f60d3a7908d294dc
windy <707298413@qq.com>
2025-04-29 03:37:17 +0800

    Create sqlx-game-sample.lua

---
+++ config/sqlx-game-sample.lua
@@ -0,0 +1,9 @@
+return {
+    host = "127.0.0.1",
+    port = 3306,
+    database = "DATABASE",
+    user = "USER",
+    password = "PASSWORD",
+    charset = "utf8mb4",
+    max_packet_size = 1024 * 1024,
+}
